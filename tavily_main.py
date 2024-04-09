468d6ae0ed1f1e9f4352752d9c7e0a487039a04b
Avik Malladi <69607237+avikMall@users.noreply.github.com>
2024-04-09 18:26:09 -0500

    Add files via upload

--- tavily_main.py
+++ tavily_main.py
@@ -1,7 +1,9 @@
 ## Getting content from tavily APIs
 
-user_query = "What are the effects of parental involvement on academic performance?"
-user_query = "How can a father and mother influence a child's performance and grades in school?"
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from tavily import TavilyClient
 import shutil
@@ -16,54 +18,72 @@ from llama_index.core import ( # type: ignore
 )
 from llama_index.llms.openai import OpenAI # type: ignore
 from typing import List, Optional
-from llama_index.llms.huggingface import ( # type: ignore
-    HuggingFaceInferenceAPI,
-    HuggingFaceLLM,
-)
+# from llama_index.llms.huggingface import ( # type: ignore
+#     HuggingFaceInferenceAPI,
+#     HuggingFaceLLM,
+# )
 
 
 from llama_index.core.response.notebook_utils import display_source_node # type: ignore
 from llama_index.core.query_engine import RetrieverQueryEngine # type: ignore
 from llama_index.core import Settings # type: ignore
 
-from dotenv import load_dotenv
+user_query = "What are the effects of parental involvement on academic performance?"
+user_query = "How can a father and mother influence a child's performance and grades in school?"
 
-load_dotenv()
+class FactFlow:
+    def __init__(self, query):
+        self.user_query = query
 
-tavily = TavilyClient(api_key=os.getenv('TAVILY_API_KEY'))
+    def get_response(self):
+        tavily = TavilyClient(api_key=os.getenv('TAVILY_API_KEY'))
 
-response = tavily.search(query=user_query, 
-                         search_depth="advanced", 
-                         include_raw_content=True,
-                         max_results=5)
+        response = tavily.search(query=self.user_query, 
+                                search_depth="advanced", 
+                                include_raw_content=True,
+                                max_results=5)
 
-if os.path.exists('tavily_data'):
-    shutil.rmtree('tavily_data')
-    os.makedirs('tavily_data')
-else:
-    os.makedirs('tavily_data')
+        if os.path.exists('tavily_data'):
+            shutil.rmtree('tavily_data')
+            os.makedirs('tavily_data')
+        else:
+            os.makedirs('tavily_data')
 
-for count, result in enumerate(response['results']):
-    f = open("tavily_data/demofile{}.txt".format(count), "a", encoding="utf-8")
-    f.truncate(0)
-    f.write(result['raw_content'])
-    f.close()
+        for count, result in enumerate(response['results']):
+            f = open("tavily_data/demofile{}.txt".format(count), "a", encoding="utf-8")
+            f.truncate(0)
+            f.write(result['raw_content'])
+            f.close()
 
 
-Settings.llm = OpenAI(model="gpt-3.5-turbo", api_key=os.getenv('OPENAI_API_KEY'))
+        Settings.llm = OpenAI(model="gpt-4", api_key=os.getenv('OPENAI_API_KEY'))
 
-documents = SimpleDirectoryReader("tavily_data").load_data()
-index = VectorStoreIndex.from_documents(documents)
+        documents = SimpleDirectoryReader("tavily_data").load_data()
+        index = VectorStoreIndex.from_documents(documents)
 
-# HF_TOKEN: Optional[str] = os.getenv("HUGGING_FACE_TOKEN")
-gpt4 = OpenAI(model="gpt-3.5-turbo", api_key=os.getenv('OPENAI_API_KEY'))
-query = user_query
+        # HF_TOKEN: Optional[str] = os.getenv("HUGGING_FACE_TOKEN")
+        gpt4 = OpenAI(model="gpt-4", api_key=os.getenv('OPENAI_API_KEY'))
+        query = self.user_query
 
-base_retriever = index.as_retriever(similarity_top_k=3)
-retrievals = base_retriever.retrieve(query)
-for n in retrievals:
-    display_source_node(n, source_length=1500)
-query_engine_base = RetrieverQueryEngine.from_args(base_retriever, llm=gpt4)
-response = query_engine_base.query(query)
+        base_retriever = index.as_retriever(similarity_top_k=5)
+        retrievals = base_retriever.retrieve(query)
+        ans = {}
+        for index, n in enumerate(retrievals):
+            if 'https:/' not in n.text:
+                # display_source_node(n, source_length=1500)
+                ans['Reference {}: '.format(index+1)] = {"Node ID": n.node_id, 
+                                                       "Text":n.text, 
+                                                       "Score":n.score}
+            else:
+                print("REMOVED CHUNK: ", n)
+        query_engine_base = RetrieverQueryEngine.from_args(base_retriever, llm=gpt4)
+        response = query_engine_base.query(query)
 
-print(str(response))
+        # print(str(response))
+        ans['Response'] = str(response)
+        return ans
+
+
+if __name__ == '__main__':
+    main = FactFlow("What are LLMs?")
+    print(main.get_response())
