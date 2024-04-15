e0781e3afe82226e94581de5e64b36c8ed4a02fa
arnavchopra1864 <arnav.chopra1864@gmail.com>
2024-04-15 17:01:30 -0500

    added basic frontend functionality

---
+++ fact-flow/src/components/LoginPage.js
@@ -0,0 +1,13 @@
+// src/components/LoginPage.js
+import React from 'react';
+
+function LoginPage () {
+  return (
+    <div>
+      <h1>Login Page</h1>
+        <p>Login with Google</p>
+    </div>
+  );
+};
+
+export default LoginPage;
