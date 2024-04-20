011fefc5bfebe412811e61344b6966272260c990
arnavchopra1864 <arnav.chopra1864@gmail.com>
2024-04-20 14:34:16 -0500

    got text printed to screen

--- fact-flow/src/components/QueryPage.js
+++ fact-flow/src/components/QueryPage.js
@@ -3,28 +3,44 @@ import React, {useState} from 'react';
 import './QueryPage.css'; // Make sure to create a CSS file with the same name
 
 const QueryPage = () => {
-  const [isSidebarExpanded, setIsSidebarExpanded] = useState(true); 
+  const [isSidebarExpanded, setIsSidebarExpanded] = useState(false); 
 
   const toggleSidebar = () => {
     setIsSidebarExpanded(!isSidebarExpanded); // Toggle the sidebar state
   };
 
   const [inputValue, setInputValue] = useState('');
+  // const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
   const handleSubmit = () => {
-    // alert(inputValue);
+    // setIsSubmitted(true);
+    
+    var query_link = "http://localhost:5000/query?q=" + inputValue.replace(/['"]+/g, '');
+    alert(inputValue);
     
     // send inputvalue to backend
-
-    var query_link = "http://localhost:5000/query?q=" + inputValue;
+    // send inputValue to backend without quotes
     fetch(query_link)
     .then(response => response.json())
     .then(data => {
       console.log(data);
+      var result_doc = document.getElementById("result");
+      var output = data.Response;
+
+      output += "\n\n References: \n";
+      
+      Object.keys(data).forEach(key => {
+        if (key.startsWith('Reference')) { 
+          const nodeID = data[key]['Node ID']; 
+          output += "Node ID: " + nodeID + "\n";
+        }
+      });
+
+      result_doc.innerText = output;
     })
     .catch(error => {
       console.error('There was an error!', error);
@@ -45,13 +61,14 @@ const QueryPage = () => {
             onChange={handleInputChange} // Update state on input change
         />
         <button onClick={handleSubmit}>Submit</button>
+        <div className="result" id="result"></div>
       </div>
       <div className={`sidebar ${isSidebarExpanded ? 'expanded' : 'collapsed'}`}>
-    <button className="toggle-button" onClick={toggleSidebar} style={buttonStyle}>
-      {isSidebarExpanded ? 'Collapse' : 'Expand'}
-    </button>
-    <p>Some sidebar content</p>
-  </div>
+        <button className="toggle-button" onClick={toggleSidebar} style={buttonStyle}>
+          {isSidebarExpanded ? 'Collapse' : 'Expand'}
+        </button>
+      <p>Query Folders will display here</p>
+      </div>
     </div>
   );
 };
