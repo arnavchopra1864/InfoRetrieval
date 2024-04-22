eb48187e1f6a5e28332081163c83c0af22622c72
Avik Malladi <69607237+avikMall@users.noreply.github.com>
2024-04-21 19:02:30 -0500

    added title and url elements to json response

--- flask_main.py
+++ flask_main.py
@@ -1,16 +1,36 @@
 from flask import Flask, request, jsonify
 from tavily_main import FactFlow
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use the application default credentials.
+import firebase_admin
+from firebase_admin import credentials
+import uuid
+
 app = Flask(__name__)
+
+cred = credentials.Certificate("./creds.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+fact_flow = FactFlow()
+
+def add_ref(response):
+    db.collection("users").document(str(uuid.uuid4())).set(response)
+
 
 @app.route('/query')
 def print_query():
     query = request.args.get('q')
     # response=""
-    new_query = FactFlow(query)
-    response = new_query.get_response()
+    response = fact_flow.get_response(query)
     # return '''Query: {}
     #         Response: {}''' .format(query, response)
+    add_ref(response)
     return response
 
 if __name__ == '__main__':
--- tavily_main.py
+++ tavily_main.py
@@ -32,38 +32,44 @@ user_query = "What are the effects of parental involvement on academic performan
 user_query = "How can a father and mother influence a child's performance and grades in school?"
 
 class FactFlow:
-    def __init__(self, query):
-        self.user_query = query
+    def __init__(self):
+        self.id = {}
 
-    def get_response(self):
+    def get_response(self, query):
         tavily = TavilyClient(api_key=os.getenv('TAVILY_API_KEY'))
 
-        response = tavily.search(query=self.user_query, 
+        response = tavily.search(query=query, 
                                 search_depth="advanced", 
                                 include_raw_content=True,
-                                max_results=5)
+                                max_results=5,
+                                exclude_domains = ['https://en.wikipedia.org/'])
 
-        if os.path.exists('tavily_data'):
-            shutil.rmtree('tavily_data')
-            os.makedirs('tavily_data')
-        else:
-            os.makedirs('tavily_data')
+        # if os.path.exists('tavily_data'):
+        #     shutil.rmtree('tavily_data')
+        #     os.makedirs('tavily_data')
+        # else:
+        #     os.makedirs('tavily_data')
 
+        input_files = []
+        meta_map = {}
         for count, result in enumerate(response['results']):
-            f = open("tavily_data/demofile{}.txt".format(count), "a", encoding="utf-8")
-            f.truncate(0)
+            n = len(self.id)
+            self.id[result['title']] = n
+            file = "tavily_data/file{}".format(n)
+            input_files.append(file)
+            meta_map[count] = {'title':result['title'], 'url':result['url']}
+            f = open(file, "a", encoding="utf-8")
             f.write(result['raw_content'])
             f.close()
 
-
+        Settings.chunk_size = 512
         Settings.llm = OpenAI(model="gpt-4", api_key=os.getenv('OPENAI_API_KEY'))
 
-        documents = SimpleDirectoryReader("tavily_data").load_data()
+        documents = SimpleDirectoryReader(input_files=input_files).load_data()
         index = VectorStoreIndex.from_documents(documents)
 
         # HF_TOKEN: Optional[str] = os.getenv("HUGGING_FACE_TOKEN")
         gpt4 = OpenAI(model="gpt-4", api_key=os.getenv('OPENAI_API_KEY'))
-        query = self.user_query
 
         base_retriever = index.as_retriever(similarity_top_k=5)
         retrievals = base_retriever.retrieve(query)
@@ -71,9 +77,13 @@ class FactFlow:
         for index, n in enumerate(retrievals):
             if 'https:/' not in n.text:
                 # display_source_node(n, source_length=1500)
+                file_name = n.metadata['file_name'][n.metadata['file_name'].rfind('/'):]
+                curr_id = int(''.join(filter(str.isdigit, file_name)))
                 ans['Reference {}: '.format(index+1)] = {"Node ID": n.node_id, 
                                                        "Text":n.text, 
-                                                       "Score":n.score}
+                                                       "Score":n.score,
+                                                       "Title":meta_map[curr_id]['title'],
+                                                       "url":meta_map[curr_id]['url']}
             else:
                 print("REMOVED CHUNK: ", n)
         query_engine_base = RetrieverQueryEngine.from_args(base_retriever, llm=gpt4)
@@ -82,8 +92,14 @@ class FactFlow:
         # print(str(response))
         ans['Response'] = str(response)
         return ans
+    
+    def get_reprompt_response(self, query, title):
+        title_id = self.id[title]
+        title_file = "tavily_data/file{}".format(title_id)
+
+        ## run through chat engine and return AI response in json file
 
 
 if __name__ == '__main__':
-    main = FactFlow("What are LLMs?")
+    main = FactFlow(user_query)
     print(main.get_response())
