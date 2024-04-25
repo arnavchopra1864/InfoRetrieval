0e9dd3d71e642209f86be05774b3baf92cc3f042
Avik Malladi <69607237+avikMall@users.noreply.github.com>
2024-04-25 17:13:22 -0500

    Add files via upload

--- flask_main.py
+++ flask_main.py
@@ -1,36 +1,36 @@
 from flask import Flask, request, jsonify
 from tavily_main import FactFlow
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-# Use the application default credentials.
-import firebase_admin
-from firebase_admin import credentials
-import uuid
-
 app = Flask(__name__)
-
-cred = credentials.Certificate("./creds.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
 fact_flow = FactFlow()
 
-def add_ref(response):
-    db.collection("users").document(str(uuid.uuid4())).set(response)
 
-
+# first route, gets document nodes + summary response
 @app.route('/query')
 def print_query():
     query = request.args.get('q')
-    # response=""
-    response = fact_flow.get_response(query)
-    # return '''Query: {}
-    #         Response: {}''' .format(query, response)
-    add_ref(response)
+    uid = request.args.get('u')
+
+    response = fact_flow.get_response(query, uid)
+    return response
+
+# gets document content
+@app.route('/search')
+def print_content():
+    docID = request.args.get('d')
+    uid = request.args.get('u')
+    content = fact_flow.get_document(docID, uid)
+
+    return content
+
+# gets AI response for specific document
+@app.route('/search/query')
+def print_new_response():
+    query = request.args.get('q')
+    docID = request.args.get('d')
+    uid = request.args.get('u')
+
+    response = fact_flow.get_reprompt_response(query, docID, uid)
     return response
 
 if __name__ == '__main__':
--- tavily_main.py
+++ tavily_main.py
@@ -1,105 +1,160 @@
-## Getting content from tavily APIs
-
-
 from dotenv import load_dotenv
+
+from tavily import TavilyClient
+import os
+
+from llama_index.core import ( # type: ignore
+    VectorStoreIndex
+)
+from llama_index.llms.openai import OpenAI # type: ignore
+from llama_index.core.query_engine import RetrieverQueryEngine # type: ignore
+from llama_index.core import Settings # type: ignore
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import Document
+from llama_index.core.chat_engine import CondenseQuestionChatEngine
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 load_dotenv()
 
-from tavily import TavilyClient
-import shutil
-import os
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('creds.json')
 
-## querying AI model
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://fact-flow-ai-default-rtdb.firebaseio.com'
+})
 
-from pathlib import Path
-from llama_index.core import ( # type: ignore
-    VectorStoreIndex,
-    SimpleDirectoryReader,
-)
-from llama_index.llms.openai import OpenAI # type: ignore
-from typing import List, Optional
-# from llama_index.llms.huggingface import ( # type: ignore
-#     HuggingFaceInferenceAPI,
-#     HuggingFaceLLM,
-# )
-
-
-from llama_index.core.response.notebook_utils import display_source_node # type: ignore
-from llama_index.core.query_engine import RetrieverQueryEngine # type: ignore
-from llama_index.core import Settings # type: ignore
-
-user_query = "What are the effects of parental involvement on academic performance?"
-user_query = "How can a father and mother influence a child's performance and grades in school?"
+# user_query = "What are the effects of parental involvement on academic performance?"
 
 class FactFlow:
     def __init__(self):
+        # maybe unnecessary?
         self.id = {}
 
-    def get_response(self, query):
+    def get_response(self, query, uid):
+        # get Tavily response
         tavily = TavilyClient(api_key=os.getenv('TAVILY_API_KEY'))
-
         response = tavily.search(query=query, 
                                 search_depth="advanced", 
                                 include_raw_content=True,
                                 max_results=5,
                                 exclude_domains = ['https://en.wikipedia.org/'])
 
-        # if os.path.exists('tavily_data'):
-        #     shutil.rmtree('tavily_data')
-        #     os.makedirs('tavily_data')
-        # else:
-        #     os.makedirs('tavily_data')
-
-        input_files = []
+        # create maps from docIDs -> title & url
         meta_map = {}
+        user_files = {}
         for count, result in enumerate(response['results']):
-            n = len(self.id)
-            self.id[result['title']] = n
-            file = "tavily_data/file{}".format(n)
-            input_files.append(file)
+            user_files['file{}'.format(count)] = result['raw_content']
             meta_map[count] = {'title':result['title'], 'url':result['url']}
-            f = open(file, "a", encoding="utf-8")
-            f.write(result['raw_content'])
-            f.close()
 
+
+        # create and update firebase db
+        ref = db.reference('DB%20Object%20name')
+        users_ref = ref.child('users')
+        users_ref.update({
+            uid: user_files
+        })
+
+
+        # set up llama index settings
         Settings.chunk_size = 512
-        Settings.llm = OpenAI(model="gpt-4", api_key=os.getenv('OPENAI_API_KEY'))
+        # Settings.llm = OpenAI(model="gpt-4", api_key=os.getenv('OPENAI_API_KEY'))
+        Settings.llm = OpenAI(model="gpt-3.5-turbo")
 
-        documents = SimpleDirectoryReader(input_files=input_files).load_data()
-        index = VectorStoreIndex.from_documents(documents)
+        text_splitter = SentenceSplitter(chunk_size=256, chunk_overlap=10)
+        Settings.text_splitter = text_splitter
 
-        # HF_TOKEN: Optional[str] = os.getenv("HUGGING_FACE_TOKEN")
-        gpt4 = OpenAI(model="gpt-4", api_key=os.getenv('OPENAI_API_KEY'))
 
+        # create llama index pipeline
+        pipeline = IngestionPipeline(
+            transformations=[
+                SentenceSplitter(chunk_size=256, chunk_overlap=24)
+            ]
+        )
+        documents = []
+        for file in ref.get()['users'][uid]:
+            documents.append(Document(text=ref.get()['users'][uid][file], id_=file, metadata={'id':file}))
+
+        nodes = pipeline.run(documents=documents)
+        index = VectorStoreIndex(nodes)
+
+        gpt4 = OpenAI(model="gpt-3.5-turbo", api_key=os.getenv('OPENAI_API_KEY'))
+
+        # retrieve nodes, and generate json response
         base_retriever = index.as_retriever(similarity_top_k=5)
         retrievals = base_retriever.retrieve(query)
-        ans = {}
+        return_ans = {}
+        i = 0
         for index, n in enumerate(retrievals):
             if 'https:/' not in n.text:
-                # display_source_node(n, source_length=1500)
-                file_name = n.metadata['file_name'][n.metadata['file_name'].rfind('/'):]
-                curr_id = int(''.join(filter(str.isdigit, file_name)))
-                ans['Reference {}: '.format(index+1)] = {"Node ID": n.node_id, 
-                                                       "Text":n.text, 
-                                                       "Score":n.score,
-                                                       "Title":meta_map[curr_id]['title'],
-                                                       "url":meta_map[curr_id]['url']}
-            else:
-                print("REMOVED CHUNK: ", n)
+                return_ans['Reference{}: '.format(i)] = {'chunk':str(n.text), 
+                'docID':str(n.metadata['id']),
+                'title':str(meta_map[int(''.join(filter(str.isdigit, n.metadata['id'])))]['title']),
+                'url':str(meta_map[int(''.join(filter(str.isdigit, n.metadata['id'])))]['url'])
+                }
+                i += 1
+                
         query_engine_base = RetrieverQueryEngine.from_args(base_retriever, llm=gpt4)
         response = query_engine_base.query(query)
+        return_ans['response'] = str(response)
 
-        # print(str(response))
-        ans['Response'] = str(response)
-        return ans
+        return return_ans
+        
     
-    def get_reprompt_response(self, query, title):
-        title_id = self.id[title]
-        title_file = "tavily_data/file{}".format(title_id)
+    # gets document content from firebase storage
+    def get_document(self, docID, uid):
+        ref = db.reference('DB%20Object%20name')
+
+        doc_content = ref.get()['users'][uid][docID]
+        
+        return doc_content
+    
+    # gives AI response for querying a specific document using chat engine
+    def get_reprompt_response(self, query, docID, uid):
+        ref = db.reference('DB%20Object%20name')
+
+        doc_content = [Document(text=ref.get()['users'][uid][docID], 
+                               id_=docID, metadata={'id':docID})]
+
+        # set up llama index settings
+        Settings.chunk_size = 512
+        # Settings.llm = OpenAI(model="gpt-4", api_key=os.getenv('OPENAI_API_KEY'))
+        Settings.llm = OpenAI(model="gpt-3.5-turbo")
+
+        text_splitter = SentenceSplitter(chunk_size=256, chunk_overlap=10)
+        Settings.text_splitter = text_splitter
+
+
+        # create llama index pipeline
+        pipeline = IngestionPipeline(
+            transformations=[
+                SentenceSplitter(chunk_size=256, chunk_overlap=24)
+            ]
+        )
+
+        nodes = pipeline.run(documents=doc_content)
+        index = VectorStoreIndex(nodes)
+
+        gpt4 = OpenAI(model="gpt-3.5-turbo", api_key=os.getenv('OPENAI_API_KEY'))
+        base_retriever = index.as_retriever(similarity_top_k=5)
+        query_engine_base = RetrieverQueryEngine.from_args(base_retriever, llm=gpt4)
+        chat_engine = CondenseQuestionChatEngine.from_defaults(query_engine=query_engine_base)
+        response = chat_engine.chat(query)
+
+        return str(response)
+
 
         ## run through chat engine and return AI response in json file
 
 
+
 if __name__ == '__main__':
-    main = FactFlow(user_query)
-    print(main.get_response())
+    pass
+    # main = FactFlow()
+    # print(main.get_reprompt_response('query', '0', '4e4bc471-eba8-43b6-9bd1-0a45c23a10d0'))
+    # print(main.download_doc('0', '4e4bc471-eba8-43b6-9bd1-0a45c23a10d0'))
+    # print(main.get_response(user_query, str(uuid.uuid4())))
