97c3347102c8154826183b0f645533a6eb7a2ae9
Laksh Gulati <gulatilaksh@outlook.com>
2024-04-28 19:49:47 -0500

    header and side bar redone

--- fact-flow/src/components/QueryChatPage.js
+++ fact-flow/src/components/QueryChatPage.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 
 function QueryChatPage() {
+  var docid = "file0";
   const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (event) => {
@@ -8,12 +9,14 @@ function QueryChatPage() {
   };
 
   const handleDocSel = (docNum) => {
-    const hostname = "http://localhost:5000/search?d=file0&u=139801c9-88f7-4cbf-829e-135feba4df6e";
-    fetch(hostname)
-    .then(response => response.json())
+    // const hostname = "http://localhost:5000/search?d=file0&u=139801c9-88f7-4cbf-829e-135feba4df6e";
+    const hostname = "https://factflow-backend-39dce4f4a0a2.herokuapp.com/search?u=139801c9-88f7-4cbf-829e-135feba4df6e&d="
+    fetch(hostname + docid)
+    .then(response => response.text())
     .then(data => {
-      var doc_text = document.getElementsByName("doctext");
+      var doc_text = document.getElementById("document");
       doc_text.innerText = data;
+      // console.log(data)
     })
     .catch(error => {
       console.error('There was an error!', error);
@@ -22,39 +25,23 @@ function QueryChatPage() {
 
 
   const handleSubmit = () => {
-    const hostname = "http://localhost:5000/query?q=";
+    const hostname = "https://factflow-backend-39dce4f4a0a2.herokuapp.com/search/query?";
     var query_link = "";
     if (inputValue.trim() === '') {
       alert('Please enter a query');
       return;
     } else {
-      query_link = hostname + inputValue.replace(/['"]+/g, '');
+      query_link = hostname + 'u=' + '139801c9-88f7-4cbf-829e-135feba4df6e' + '&d=' + docid + '&q=' + inputValue.replace(/['"]+/g, '');
+      //
       // alert(inputValue);
     }
     
     fetch(query_link)
-    .then(response => response.json())
+    .then(response => response.text())
     .then(data => {
       console.log(data);
-      var result_doc = document.getElementById("result");
-      var output = data.Response;
-      var score = data["Reference 1: "
-    ].Score;
-      if (score >= 0.8) {
-        output += "\n\n References: \n";
-      
-        Object.keys(data).forEach(key => {
-          if (key.startsWith('Reference')) {
-            console.log(key); 
-            const nodeID = data[key]['Node ID']; 
-            output += "Node ID: " + nodeID + "\n";
-          }
-        });
-        result_doc.innerText = output;
-      } else {
-        result_doc.innerText = "\n\n No relevant references found.";
-      }
-
+      var chat = document.getElementById("chatresponse");
+      chat.innerText = data;
     })
     .catch(error => {
       console.error('There was an error!', error);
@@ -63,10 +50,9 @@ function QueryChatPage() {
   };
 
   return (
-    <div>
+    <div>x
       <h1>Chat with a document</h1>
       <div>
-          <div className='doctext'>doc  goes here</div>
         <div>
           <div>
             {Array.from({ length: 5 }, (_, index) => (
@@ -84,7 +70,9 @@ function QueryChatPage() {
           onChange={handleInputChange}
         />
         <button onClick={handleSubmit}>Send</button>
+        <div id="chatresponse">response goes here</div>
       </div>
+      <div className='doctext' id="document"> doc  goes here</div>
     </div>
   );
 }
