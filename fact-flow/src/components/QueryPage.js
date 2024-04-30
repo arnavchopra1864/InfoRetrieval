a58c91d35702ca857339d9dcc6246df8581f457f
Matthew Tolea <matthewtolea@gmail.com>
2024-04-30 16:51:54 -0500

    added error display

--- fact-flow/src/components/QueryPage.js
+++ fact-flow/src/components/QueryPage.js
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import Lottie from 'lottie-react';
-import animationData from './loading-animation.json';
-import '../styles/QueryPage.css';
-import FileView from './FileView.js';
-import RefCard from './query_Page/refCard.js';
-import ResponseCard from './query_Page/responseCard.js';
-import './query_Page/search.scss';
-import logoMain from './logo_main.png';
-import logo from './logoblack.png';
+import React, { useState, useEffect } from "react";
+import Lottie from "lottie-react";
+import animationData from "./loading-animation.json";
+import "../styles/QueryPage.css";
+import FileView from "./FileView.js";
+import RefCard from "./query_Page/refCard.js";
+import ResponseCard from "./query_Page/responseCard.js";
+import "./query_Page/search.scss";
+import logoMain from "./logo_main.png";
+import logo from "./logoblack.png";
+import { MdErrorOutline } from "react-icons/md";
 
 const QueryPage = () => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [response, setResponse] = useState();
   const [references, setReferences] = useState([]);
-  const [initialQuery, setInitialQuery] = useState('');
+  const [initialQuery, setInitialQuery] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [abortController, setAbortController] = useState(null);
   const [isSearch, setIsSearch] = useState(true);
+  const [error, setError] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -32,53 +34,59 @@ const QueryPage = () => {
     const newAbortController = new AbortController();
     setAbortController(newAbortController);
 
+    setError(false);
     setIsLoading(true);
     setIsSearch(false);
-    setInitialQuery(inputValue);
+    setResponse("");
+    setReferences([]);
 
-    const hostname = "https://factflow-backend-39dce4f4a0a2.herokuapp.com/query?q=";
+    const hostname =
+      "https://factflow-backend-39dce4f4a0a2.herokuapp.com/query?q=";
     // const hostname = "http://127.0.0.1:8000/query?q=";
     let query_link = "http://127.0.0.1:5000/query?q=";
 
-    if (inputValue.trim() === '') {
+    if (inputValue.trim() === "") {
       console.log("Please enter a query");
       setIsLoading(false);
       return;
     } else {
-      query_link = hostname + inputValue.replace(/['"]+/g, '') + '&u=' + Date.now();
+      query_link =
+        hostname + inputValue.replace(/['"]+/g, "") + "&u=" + Date.now();
+      setInitialQuery(inputValue);
     }
 
     fetch(query_link, { signal: newAbortController.signal })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setResponse(data.response);
         // console.log(data)
 
         const refs = [];
         Object.keys(data).forEach((key) => {
-          if (key.startsWith('Reference')) {
-            const text = data[key]['chunk'];
-            const url = data[key]['url'];
-            const title = data[key]['title'];
+          if (key.startsWith("Reference")) {
+            const text = data[key]["chunk"];
+            const url = data[key]["url"];
+            const title = data[key]["title"];
             refs.push({ key, text, url, title });
           }
         });
         setReferences(refs);
-        setInputValue('');
+        setInputValue("");
         setIsLoading(false);
       })
-      .catch(error => {
-        if (error.name === 'AbortError') {
-          console.log('Request aborted');
+      .catch((error) => {
+        if (error.name === "AbortError") {
+          console.log("Request aborted");
         } else {
-          console.error('There was an error!', error);
+          console.error("There was an error!", error);
           setIsLoading(false);
+          setError(true);
         }
-      })
+      });
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSubmit();
     }
   };
@@ -91,16 +99,24 @@ const QueryPage = () => {
 
   const renderResponseCard = () => {
     return response && references.length > 0 ? (
-      <ResponseCard query={initialQuery} text={response} className='resp-card' />
+      <ResponseCard
+        query={initialQuery}
+        text={response}
+        className="resp-card"
+      />
     ) : null;
   };
   return (
     <div className="container">
       <div className="content">
         {isSearch && (
-          <img className='logo-main' src={logo} style={{height:'40vh'}}></img>
+          <img
+            className="logo-main"
+            src={logo}
+            style={{ height: "40vh" }}
+          ></img>
         )}
-        <div className='search-bar'>
+        <div className="search-bar">
           <input
             type="search"
             placeholder="Search..."
@@ -110,22 +126,26 @@ const QueryPage = () => {
           />
           <button onClick={handleSubmit}>Go</button>
         </div>
+        {error && (
+          <>
+            <MdErrorOutline className="icon-error" />
+            <p className="error">Oops! An error occurred. Please try again.</p>
+          </>
+        )}
         {isLoading && (
           <div className="loading-container">
-            <Lottie animationData={animationData} style={{height: '75vh'}}/>
+            <Lottie animationData={animationData} style={{ height: "75vh" }} />
           </div>
         )}
         {!isLoading && (
           <>
-            <div className='result'>
-              {renderResponseCard()}
-            </div>
+            <div className="result">{renderResponseCard()}</div>
             {references.length > 0 && (
-              <div className='cards-container'>
-                <h1><strong>References:</strong></h1>
-                <div className='reference-container'>
-                  {renderRefCards()}
-                </div>
+              <div className="cards-container">
+                <h1>
+                  <strong>References:</strong>
+                </h1>
+                <div className="reference-container">{renderRefCards()}</div>
               </div>
             )}
           </>
